a72a998fb99db96d2e2a067c6fb09aee69a58ecd
Dave Shanley <dave@quobix.com>
2023-02-10 09:34:22 -0500

    removed headers.

--- plank/pkg/middleware/basic_security_headers.go
+++ plank/pkg/middleware/basic_security_headers.go
@@ -4,17 +4,14 @@
 package middleware
 
 import (
-	"github.com/gorilla/mux"
-	"net/http"
+    "github.com/gorilla/mux"
+    "net/http"
 )
 
 func BasicSecurityHeaderMiddleware() mux.MiddlewareFunc {
-	return func(handler http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("X-Content-Type-Options", "no-sniff")
-			w.Header().Set("X-Frame-Options", "SAMEORIGIN")
-			w.Header().Set("X-Xss-Protection", "1; mode=block")
-			handler.ServeHTTP(w, r)
-		})
-	}
+    return func(handler http.Handler) http.Handler {
+        return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+            handler.ServeHTTP(w, r)
+        })
+    }
 }
