b6df5f0825852d35dac3f254c8d0b5ed1da02276
Dave Shanley <dave@quobix.com>
2023-02-11 10:03:50 -0500

    Adding frame options, they are being overridden by railway.

--- plank/pkg/middleware/basic_security_headers.go
+++ plank/pkg/middleware/basic_security_headers.go
@@ -11,6 +11,7 @@ import (
 func BasicSecurityHeaderMiddleware() mux.MiddlewareFunc {
     return func(handler http.Handler) http.Handler {
         return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+            w.Header().Set("X-Frame-Options", "allow-from https://pb33f.io/")
             handler.ServeHTTP(w, r)
         })
     }
